7a0bce414f8c71ea242b5f94eac5e46112b56875
s2feeling <s2feeling@naver.com>
2022-08-30 21:18:15 +0900

    #14 Test, Login, Main, TestScreen 파일 수정

--- front/src/component/Login.js
+++ front/src/component/Login.js
@@ -1,5 +1,52 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Layout from "../layout/Layout";
+import styled from "styled-components";
+
+
+const Div = styled.div`
+    height: 999px;
+    background-color: #e9e9e9;
+`
+
+const InputBoxes = styled.div`
+    width: 500px;
+    height: 600px;
+    display: flex;
+    flex-direction: column;
+    margin: 0 auto;
+    padding-top: 120px;
+`
+
+const H4 = styled.h4`
+    font-weight: bold;
+    height: 10px;
+`
+
+const Input = styled.input`
+    width: 300px;
+    height: 30px;
+    margin-left: 100px;
+    border-color: grey;
+    border-width: 1px;
+    border-radius: 3px;
+    padding-left: 2px;
+`
+
+const Button = styled.button`
+    width: 305px;
+    height: 35px;
+    margin-top: 40px;
+    margin-left: 100px;
+    cursor: pointer;
+    background-color: dodgerblue; 
+    border-radius: 3px;
+    border-width: 2px;
+    border-style: solid;
+    border-color: dodgerblue;
+    color: white;
+    font-weight: bold;
+`
 
 
 const Login = () => {
@@ -40,25 +87,29 @@ const Login = () => {
 
 
     return(
-        <div>
-        이메일
-        <input
-          type="email"
-          id="email"
-          placeholder="Enter email"
-          value={email}
-          onChange={handleEmailChange}
-        />
-        비밀번호
-        <input
-          type="password"
-          id="password"
-          placeholder="Password"
-          value={password}
-          onChange={handlePasswordChange}
-        />
-        <button onClick={onSubmit}>로그인</button>
-      </div>
+        <Layout>
+            <Div>
+                <InputBoxes>
+                    <H4>이메일</H4>
+                    <Input
+                    type="email"
+                    id="email"
+                    placeholder="Enter email"
+                    value={email}
+                    onChange={handleEmailChange}
+                    />
+                    <H4>비밀번호</H4>
+                    <Input
+                    type="password"
+                    id="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                    />
+                    <Button onClick={onSubmit}>로그인</Button>
+                </InputBoxes>
+            </Div>
+      </Layout>
     );
 
 
