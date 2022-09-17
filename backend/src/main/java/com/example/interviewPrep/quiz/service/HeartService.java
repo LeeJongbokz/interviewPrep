2e35c249355c1b964d9c6566af4d6f290abae274
wjdxor <wjdxortaek@gmail.com>
2022-09-17 21:04:38 +0900

    refact: orElseThrow()로 가져온 Optional에 대하여 예외 메시지작성

--- backend/src/main/java/com/example/interviewPrep/quiz/service/HeartService.java
+++ backend/src/main/java/com/example/interviewPrep/quiz/service/HeartService.java
@@ -2,6 +2,8 @@ package com.example.interviewPrep.quiz.service;
 
 import com.example.interviewPrep.quiz.domain.Answer;
 import com.example.interviewPrep.quiz.domain.Heart;
+import com.example.interviewPrep.quiz.exception.AnswerNotFoundException;
+import com.example.interviewPrep.quiz.exception.HeartNotFountException;
 import com.example.interviewPrep.quiz.repository.AnswerRepository;
 import com.example.interviewPrep.quiz.repository.HeartRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,7 +20,8 @@ public class HeartService {
     private final AnswerRepository answerRepository;
 
     public Heart createHeart(Long answerId) {
-        Answer answer = answerRepository.findById(answerId).orElseThrow();
+        Answer answer = answerRepository.findById(answerId).orElseThrow(() ->
+                new AnswerNotFoundException("답변 정보를 찾을 수 없어 좋아요를 누를 수 없습니다."));
         //TODO 멤버 정보 가져오기 - 좋아요 기록 검증
         Heart heart = Heart.builder()
                 .answer(answer)
@@ -31,7 +34,8 @@ public class HeartService {
 
     public Heart deleteHeart(Long heartId) {
         //TODO 멤버 정보 가져오기 - 좋아요 기록 검증
-        Heart heart = heartRepository.findById(heartId).orElseThrow();
+        Heart heart = heartRepository.findById(heartId).orElseThrow(() ->
+                new HeartNotFountException("좋아요를 누른 기록이 없어 좋아요취소를 할 수 없습니다."));
         heartRepository.delete(heart);
         return heart;
     }
