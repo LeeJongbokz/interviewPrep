079908b15f0eb2b2954844b7db96b2a94919102a
LeeJongbokZ <s2feeling@naver.com>
2022-09-17 19:41:34 +0900

    #25 AnswerController에서 답안 생성 메소드를 복수-> 단수로 변경, AnswerJpaRepository 클래스 삭제,AnswerService 클래스 수정

--- backend/src/main/java/com/example/interviewPrep/quiz/service/AnswerService.java
+++ backend/src/main/java/com/example/interviewPrep/quiz/service/AnswerService.java
@@ -2,20 +2,13 @@ package com.example.interviewPrep.quiz.service;
 
 
 import com.example.interviewPrep.quiz.domain.Answer;
-import com.example.interviewPrep.quiz.domain.Member;
 import com.example.interviewPrep.quiz.domain.Question;
 import com.example.interviewPrep.quiz.dto.AnswerDTO;
-import com.example.interviewPrep.quiz.dto.QuestionDTO;
-import com.example.interviewPrep.quiz.repository.AnswerJpaRepository;
 import com.example.interviewPrep.quiz.repository.AnswerRepository;
-import com.example.interviewPrep.quiz.repository.QuestionJpaRepository;
 import com.example.interviewPrep.quiz.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -23,41 +16,18 @@ import java.util.Optional;
 public class AnswerService {
 
     private final AnswerRepository answerRepository;
-    private final AnswerJpaRepository answerJpaRepository;
-    private final QuestionJpaRepository questionJpaRepository;
-
-    public List<Answer> createAnswers(List<AnswerDTO> answerDTOs){
-
-        List<Answer> answers = new ArrayList<>();
-
-        for(AnswerDTO answerDTO: answerDTOs){
-
-            Question question = new Question();
-            question.setId(answerDTO.getQuestionId());
-
-            Answer answer =  Answer.builder()
-                    .question(question)
-                    .content(answerDTO.getContent())
-                    .build();
-
-            answers.add(answer);
-        }
-
-        answerRepository.save(answers);
-        return answers;
-    }
-
+    private final QuestionRepository questionRepository;
     public Answer createAnswer(AnswerDTO answerDTO){
 
 
-        Optional<Question> question = questionJpaRepository.findById(answerDTO.getQuestionId());
+        Optional<Question> question = questionRepository.findById(answerDTO.getQuestionId());
 
         Answer answer =  Answer.builder()
                 .question(question.get())
                 .content(answerDTO.getContent())
                 .build();
 
-        answerJpaRepository.save(answer);
+        answerRepository.save(answer);
         return answer;
     }
 
