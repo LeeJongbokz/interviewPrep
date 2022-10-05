48d096101427d291c039ea5b0a547e48d58d949d
LeeJongbokZ <s2feeling@naver.com>
2022-10-05 18:14:48 +0900

    #38 로그인 기능 수정(로깅 추가, CORS 주소 변경, 예외 조건 추가)

--- backend/src/main/java/com/example/interviewPrep/quiz/controller/MemberController.java
+++ backend/src/main/java/com/example/interviewPrep/quiz/controller/MemberController.java
@@ -5,6 +5,9 @@ import com.example.interviewPrep.quiz.dto.LoginRequestDTO;
 import com.example.interviewPrep.quiz.dto.LoginResponseDTO;
 import com.example.interviewPrep.quiz.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,20 +15,35 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.constraints.NotNull;
 
-@RestController
+@RestController("/login")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://52.3.173.210")
+@Slf4j
 public class MemberController {
     private final AuthenticationService authService;
-    @PostMapping("/login")
-    public LoginResponseDTO login(@RequestBody @NotNull LoginRequestDTO member){
+    @PostMapping
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody @NotNull LoginRequestDTO member){
 
-        String email = member.getEmail();
-        String password = member.getPassword();
+        ResponseEntity<LoginResponseDTO> responseEntity = null;
+        String token = "";
 
-        String token = authService.login(email, password);
+        if(member == null){
+            responseEntity = new ResponseEntity<>(toResponse(token), HttpStatus.UNAUTHORIZED);
+        }else{
 
-        return toResponse(token);
+            try {
+                String email = member.getEmail();
+                String password = member.getPassword();
+
+                token = authService.login(email, password);
+                responseEntity = new ResponseEntity<>(toResponse(token), HttpStatus.UNAUTHORIZED);
+            }catch(RuntimeException re){
+                log.error("login Error:" + responseEntity);
+            }
+        }
+
+
+        return responseEntity;
     }
 
     private LoginResponseDTO toResponse(String token){
