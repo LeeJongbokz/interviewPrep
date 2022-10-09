8476beec2f5bc164e4735cc062bc2c033e79c2a1
JT <38105420+wjdxor@users.noreply.github.com>
2022-10-10 00:01:04 +0900

    fix: respository 변경

--- backend/src/main/java/com/example/interviewPrep/quiz/domain/HeartRepository.java
+++ backend/src/main/java/com/example/interviewPrep/quiz/domain/HeartRepository.java
@@ -1,11 +1,5 @@
 package com.example.interviewPrep.quiz.domain;
 
-import java.util.Optional;
-
 public interface HeartRepository {
-    Heart save(Heart heart);
-
-    Optional<Heart> findById(Long id);
-
-    void delete(Heart heart);
+    int findByAnswerId(Long id);
 }
--- backend/src/main/java/com/example/interviewPrep/quiz/service/HeartService.java
+++ backend/src/main/java/com/example/interviewPrep/quiz/service/HeartService.java
@@ -3,22 +3,21 @@ package com.example.interviewPrep.quiz.service;
 import com.example.interviewPrep.quiz.domain.Answer;
 import com.example.interviewPrep.quiz.domain.AnswerRepository;
 import com.example.interviewPrep.quiz.domain.Heart;
-import com.example.interviewPrep.quiz.domain.HeartRepository;
 import com.example.interviewPrep.quiz.exception.AnswerNotFoundException;
 import com.example.interviewPrep.quiz.exception.HeartNotFountException;
+import com.example.interviewPrep.quiz.infra.JpaHeartRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class HeartService {
-    private final HeartRepository heartRepository;
+    private final JpaHeartRepository heartRepository;
     private final AnswerRepository answerRepository;
 
     public Heart createHeart(Long answerId) {
         Answer answer = answerRepository.findById(answerId).orElseThrow(() ->
             new AnswerNotFoundException("답변 정보를 찾을 수 없어 좋아요를 누를 수 없습니다."));
-
         //TODO 멤버 정보 가져오기 - 좋아요 기록 검증
         Heart heart = Heart.builder()
             .answer(answer)
