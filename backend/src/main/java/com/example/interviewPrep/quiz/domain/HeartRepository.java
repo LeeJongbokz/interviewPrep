db3ae70fe65db92c53c8279950d9eefdf376b912
LeeJongbokZ <s2feeling@naver.com>
2022-10-20 00:19:39 +0900

    #51 Repository 폴더 생성

--- backend/src/main/java/com/example/interviewPrep/quiz/domain/HeartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.interviewPrep.quiz.domain;
-
-import java.util.List;
-
-public interface HeartRepository {
-    List<Heart> findByAnswerId(Long id);
-    int countHeartByAnswerId(long id);
-}
