f0684993e42928001e4579a20c51fbba1931d162
LeeJongbokZ <s2feeling@naver.com>
2022-10-24 15:45:13 +0900

    #58-2 QuestionReadWebController 테스트 수정

--- backend/src/test/java/com/example/interviewPrep/quiz/Question/controller/QuestionReadWebControllerTest.java
+++ backend/src/test/java/com/example/interviewPrep/quiz/Question/controller/QuestionReadWebControllerTest.java
@@ -2,7 +2,6 @@ package com.example.interviewPrep.quiz.Question.controller;
 
 import com.example.interviewPrep.quiz.controller.QuestionController;
 import com.example.interviewPrep.quiz.domain.Question;
-import com.example.interviewPrep.quiz.domain.QuestionRepository;
 import com.example.interviewPrep.quiz.dto.QuestionDTO;
 import com.example.interviewPrep.quiz.service.QuestionService;
 import org.junit.jupiter.api.BeforeEach;
