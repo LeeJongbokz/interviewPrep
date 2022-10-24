86546a20e6e9fe3384a32f2eefae93a029f341bb
LeeJongbokZ <s2feeling@naver.com>
2022-10-24 15:43:05 +0900

    #58-1 테스트 import문 수정

--- backend/src/test/java/com/example/interviewPrep/quiz/Member/repository/MemberRepositoryTest.java
+++ backend/src/test/java/com/example/interviewPrep/quiz/Member/repository/MemberRepositoryTest.java
@@ -1,30 +1,29 @@
 package com.example.interviewPrep.quiz.Member.repository;
 
 import com.example.interviewPrep.quiz.domain.Member;
-import com.example.interviewPrep.quiz.domain.MemberRepository;
+import com.example.interviewPrep.quiz.infra.JpaMemberRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
 @Transactional
+@Rollback(value = false)
 class MemberRepositoryTest {
 
-    @Autowired
-    private final MemberRepository memberRepository = mock(MemberRepository.class);
-
+    @Autowired private JpaMemberRepository memberRepository;
 
     Member member;
     String email;
@@ -34,12 +33,15 @@ class MemberRepositoryTest {
     @BeforeEach
     void setUp(){
 
+
+
         // Given
         email = "hello@gmail.com";
         password = "1234";
         type = "google";
 
         member = Member.builder()
+                .id(3L)
                 .email(email)
                 .password(password)
                 .type(type)
@@ -53,7 +55,8 @@ class MemberRepositoryTest {
     public void save(){
 
         // When
-        memberRepository.save(member);
+        Member savedMember = memberRepository.save(member);
+        System.out.println("savedMember는?" + savedMember);
 
         // Then
         String savedEmail = member.getEmail();
