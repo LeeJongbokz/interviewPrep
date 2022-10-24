18dce36f1dcd9b028102e07d11806bde35692ee9
JT <38105420+wjdxor@users.noreply.github.com>
2022-10-24 17:08:42 +0900

    test: heart test수정 (#97)
    * test: test수정
    
    * test: mock 중복 생성 삭제

--- backend/src/test/java/com/example/interviewPrep/quiz/Heart/service/HeartServiceTest.java
+++ backend/src/test/java/com/example/interviewPrep/quiz/Heart/service/HeartServiceTest.java
@@ -1,58 +1,55 @@
 package com.example.interviewPrep.quiz.Heart.service;
 
 import com.example.interviewPrep.quiz.domain.Answer;
-import com.example.interviewPrep.quiz.repository.AnswerRepository;
 import com.example.interviewPrep.quiz.domain.Heart;
 import com.example.interviewPrep.quiz.exception.AnswerNotFoundException;
 import com.example.interviewPrep.quiz.exception.HeartNotFountException;
+import com.example.interviewPrep.quiz.repository.AnswerRepository;
 import com.example.interviewPrep.quiz.repository.HeartRepository;
 import com.example.interviewPrep.quiz.service.HeartService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
 import java.util.Optional;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 import static org.junit.Assert.assertThrows;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class HeartServiceTest {
 
-    private HeartRepository heartRepository = mock(HeartRepository.class);
-    @Autowired
+    @Mock
+    HeartRepository heartRepository;
+    @Mock
     AnswerRepository answerRepository;
-    HeartService heartService;
 
-    Answer answer;
+    HeartService heartService;
 
     @BeforeEach
     void setUp() {
-        answer = Answer.builder().build();
-        answerRepository.save(answer);
         heartService = new HeartService(heartRepository, answerRepository);
 
-        Heart heart = Heart.builder()
-                .id(1L)
-                .build();
+        Answer answer = Answer.builder()
+            .id(1L)
+            .build();
 
-        when(heartRepository.findById(1L)).thenReturn(Optional.of(heart));
+        when(answerRepository.findById(1L)).thenReturn(Optional.of(answer));
     }
 
-    // 테스트 완료
     @Test
     @DisplayName("좋아요 테스트")
     void create() {
-        heartService.createHeart(answer.getId());
+        Heart savedHeart = heartService.createHeart(1L);
 
-        verify(heartRepository).save(any(Heart.class));
+        verify(heartRepository).save(savedHeart);
     }
 
-    // 테스트 완료
     @Test
     @DisplayName("좋아요를 눌렀는데 답변이 없을경우, AnswerNotFoundException가 발생한다.")
     void create_notFoundAnswer_test() {
@@ -61,44 +58,24 @@ public class HeartServiceTest {
         });
     }
 
-
-    // 테스트 완료
     @Test
     @DisplayName("좋아요 취소 테스트")
     void delete() {
+        Heart heart = Heart.builder()
+            .id(1L)
+            .build();
+        when(heartRepository.findById(1L)).thenReturn(Optional.of(heart));
+
         heartService.deleteHeart(1L);
 
         verify(heartRepository).delete(any(Heart.class));
     }
 
-    // 테스트 완료
     @Test
     @DisplayName("좋아요 취소를 눌렀는데 좋아요가 없다면, HeartNotFountException가 발생한다.")
     void delete_notFoundHeart_test() {
         assertThrows(HeartNotFountException.class, () -> {
             heartService.deleteHeart(-1L);
         });
-    }
-
-    @Test
-    @DisplayName("하나의 답변에 동시에 좋아요가 눌렸을때, ")
-    void concurrency_test() throws InterruptedException {
-        int threadCount = 100;
-        ExecutorService executorService = Executors.newFixedThreadPool(32);
-        CountDownLatch latch = new CountDownLatch(threadCount);
-
-        for (int i = 0; i < threadCount; i++) {
-            executorService.execute(() -> {
-                try {
-                    heartService.createHeart(answer.getId());
-                } finally {
-                    latch.countDown();
-                }
-            });
-
-        }
-        latch.await();
-
-        // assertEquals(heartRepository.countHeartByAnswerId(answer.getId())).isEqualTo(100);
     }
 }
