efb4edc14bd5155a8076979962bbf1744d1abf84
JT <38105420+wjdxor@users.noreply.github.com>
2022-10-26 09:41:47 +0900

    feat: heartService - heartIncrease 구현

--- backend/src/main/java/com/example/interviewPrep/quiz/service/HeartService.java
+++ backend/src/main/java/com/example/interviewPrep/quiz/service/HeartService.java
@@ -8,6 +8,7 @@ import com.example.interviewPrep.quiz.exception.HeartNotFountException;
 import com.example.interviewPrep.quiz.repository.HeartRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +25,8 @@ public class HeartService {
             .answer(answer)
             .build();
 
+        increaseHeartWithOptimisticLock(answer.getId());
+
         heartRepository.save(heart);
         return heart;
     }
@@ -36,5 +39,15 @@ public class HeartService {
         heartRepository.delete(heart);
 
         return heart;
+    }
+
+    @Transactional
+    public void increaseHeartWithOptimisticLock(Long answerId) {
+        Answer answer = answerRepository.findByIdWithOptimisticLock(answerId).orElseThrow(() ->
+            new AnswerNotFoundException("답변 정보를 찾을 수 없어 좋아요를 누를 수 없습니다."));
+
+        answer.increase();
+
+        answerRepository.saveAndFlush(answer);
     }
 }
