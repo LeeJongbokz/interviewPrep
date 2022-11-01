781f2af972d63a2fc5340f87a3756778b03a0aa6
N3theri9N <nnea5215@gmail.com>
2022-11-01 15:51:01 +0900

    testScreen 화면 기능 구현 ( firebase 는 임시로 )

--- frontend/src/component/TestScreen.js
+++ frontend/src/component/TestScreen.js
@@ -1,88 +1,103 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import { subjects } from '../utils/data';
-import * as API from '../utils/api';
+import { useParams } from 'react-router-dom';
+// import { subjects } from '../utils/data';
+// import * as API from '../utils/api';
 import Container from "@mui/material/Container";
-import FormControl from "@mui/material/FormControl"
-import Input from "@mui/material/Input"
-import InputLabel from "@mui/material/Input"
-import Typography from "@mui/material/Typography"
-import Button from "@mui/material/Button"
+import FormControl from "@mui/material/FormControl";
+import TextField from "@mui/material/TextField";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import PaperUI from './UI/PaperUI';
 
 const TestScreen = () => {
-  const [questions, setQuestions] = useState([]);
-  const [answerDTOs] = useState([]);
+  const { subject:testId } = useParams();
 
-  const location = useLocation();
-  const paths = location.pathname.split('/');
-  let type = paths[2];
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
 
-  let subject = subjects.map(subject => {
-    return subject[0] === paths[2] ? subject[1] : '';
-  });
+  // const location = useLocation();
+  // const paths = location.pathname.split('/');
+  // let type = paths[2];
 
-  const handleChange = (e, id) => {
-    let answerDTO = {
-      content: e.target.value,
-      questionId: id,
-    };
-    answerDTOs[id - 1] = answerDTO;
+  // let subject = subjects.map(subject => {
+  //   return subject[0] === paths[2] ? subject[1] : '';
+  // });
+
+  const changeHandler = (e) => {
+    setAnswer(e.target.value);
   };
 
-  const onSubmit = () => {
+  const submitHandler = async () => {
     if (window.confirm('답안을 제출하시겠습니까?')) {
-      try {
-        const res = API.post('answer', {
-          answers: answerDTOs,
-        });
-        if (res.status === 200) {
-          alert('답안 제출이 완료되었습니다.');
-          // window.location.href="/test";
-        }
-      } catch (error) {
-        alert('답안이 제출되지 않았습니다.');
+      const bodyData = {
+        testId,
+        answer
       }
+      const response = await fetch('https://react-post-de8f7-default-rtdb.firebaseio.com/interviewPrep/answer.json', {
+        method: 'POST',
+        body: JSON.stringify(bodyData), 
+        headers: {
+          'Content-Type' : 'application/json'
+        }
+      })
+      if(!response.ok){
+        alert("오류가 발생했습니다. 다시 시도해주세요!")
+        return;
+      }     
+      // redirect to where??
     }
   };
 
   useEffect(() => {
-      // try {
-      //   const res = API.get('question/' + type, {});
-      //   if (res.status === 200) {
-      //     if (questions.length === 0) {
-      //       setQuestions(res.data);
-      //     }
-      //   }
-      // } catch (err) {
-      //   console.log(err);
-      // }
-      setQuestions([{title:"title"},{title:"titie12"}]);
+    // try {
+    //   const res = API.get('question/' + type, {});
+    //   if (res.status === 200) {
+    //     if (questions.length === 0) {
+    //       setQuestions(res.data);
+    //     }
+    //   }
+    // } catch (err) {
+    //   console.log(err);
+    // }
+
+    const fetchQuestion = async () => {
+      console.log(testId);
+      const response = await fetch(`https://react-post-de8f7-default-rtdb.firebaseio.com/interviewPrep/question/${testId}.json`);
+
+      if(!response.ok){
+        throw new Error('Some Thing Went Error');
+      }
+      const data = await response.json();
+      setQuestion(data.title);  
+    }
+    fetchQuestion().catch((err) => {
+      console.log(err)
+    })
+
+    // setQuestion([{title:"title"},{title:"titie12"}]);
   }, []);
-  console.log(type);
       
   return (
-    <Container>
-      <Typography variant="h3" gutterBottom>
-        {subject} 테스트
-      </Typography>
-        {questions.map((question, index) => (
-          <FormControl margin='dense' fullWidth key={index} variant="standard"> 
-            <InputLabel htmlFor="component-simple">
-              {`${index+1}. ${question.title}`}
-            </InputLabel>
-            <Input
-              id={`question_${index}`}
-              onChange={e => handleChange(e, question.id)} 
-              multiline
-              placeholder="답을 입력해주세요"
-            />
-          </FormControl>
-        ))}
-      <Button 
-        size="small"
-        variant="contained" 
-        onClick={onSubmit}
-      >제출하기</Button>
+    <Container maxWidth="sm">
+      <PaperUI>
+        <Typography variant="h3" gutterBottom>
+          
+        </Typography>
+        <FormControl margin='dense' fullWidth variant="standard"> 
+          <TextField id="answer"
+            multiline
+            label={`문제 : ${question}`}
+            value={answer}
+            onChange={changeHandler} 
+            placeholder="답을 입력해주세요"
+          />
+        </FormControl>
+        <Button 
+          size="small"
+          variant="contained" 
+          onClick={submitHandler}
+        >제출하기</Button>
+      </PaperUI>
     </Container>
   );
 };
