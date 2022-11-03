b6ab08e11b7176ba3d530c567ad529c3159160ff
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2022-11-03 15:40:29 +0900

    Feat: "testList api render"

--- frontend/src/component/Test.js
+++ frontend/src/component/Test.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 //import styled from 'styled-components';
 import Problem from './Problem';
 
@@ -17,9 +17,29 @@ const Test = () => {
     ['네트워크', 'OSI 7계층을 설명하시오', '상', '5명'],
   ];
 
+  const [question, setQuestion] = useState("");
+  
+  useEffect(() => {
+    const fetchQuestion = async () => {
+      console.log('api연동성공');
+      const response = await fetch(`https://react-post-de8f7-default-rtdb.firebaseio.com/interviewPrep/question.json`);
+      
+      if(!response.ok){
+        throw new Error('Some Thing Went Error');
+      }
+      const data = await response.json();
+      setQuestion(data);  
+    }
+    fetchQuestion().catch((err) => {
+      console.log(err)
+    })
+  }, []);
+
+
   return (
     <Container>
       <div>
+        
       <Grid 
         container 
         spacing={1} 
@@ -29,6 +49,7 @@ const Test = () => {
           return (
             <Grid key={index} item xs={12} sm={6} md={3}>
               <Problem problem={problem} />
+              <div> {question.title} </div>
             </Grid>
            )
         })}
