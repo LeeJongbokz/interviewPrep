6ab1b8e1e2ae78096a83abefcfcec4be135db3d7
JT <38105420+wjdxor@users.noreply.github.com>
2022-11-05 21:43:45 +0900

    fix: heartController 초기화

--- backend/src/main/java/com/example/interviewPrep/quiz/controller/HeartController.java
+++ backend/src/main/java/com/example/interviewPrep/quiz/controller/HeartController.java
@@ -1,18 +1,15 @@
 package com.example.interviewPrep.quiz.controller;
 
-import com.example.interviewPrep.quiz.dto.HeartRequestDTO;
 import com.example.interviewPrep.quiz.service.HeartService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.constraints.NotNull;
 
 import static com.example.interviewPrep.quiz.utils.ResponseEntityConstants.RESPONSE_CREATED;
 import static com.example.interviewPrep.quiz.utils.ResponseEntityConstants.RESPONSE_OK;
@@ -26,27 +23,27 @@ import static com.example.interviewPrep.quiz.utils.ResponseEntityConstants.RESPO
 public class HeartController {
     private final HeartService heartService;
 
-    @DeleteMapping
-    public ResponseEntity<Void> delete(@RequestBody @NotNull HeartRequestDTO heartDTO) {
-
+    @PostMapping("/{id}")
+    public ResponseEntity<Void> create(@PathVariable Long id) {
+        Long answerId = id;
         try {
-            heartService.deleteHeart(heartDTO);
+            heartService.createHeart(answerId);
+            return RESPONSE_CREATED;
+        } catch (Exception e) {
+            return RESPONSE_SERVER_ERROR;
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        Long answerId = id;
+        try {
+            heartService.deleteHeart(answerId);
             return RESPONSE_OK;
         } catch (Exception e) {
             return RESPONSE_SERVER_ERROR;
         }
     }
 
-    @PostMapping
-    public ResponseEntity<Void> create(@RequestBody @NotNull HeartRequestDTO heartDTO) {
-
-        try {
-            heartService.createHeart(heartDTO);
-            return RESPONSE_CREATED;
-        } catch (Exception e) {
-            log.error("error is" + e.getMessage());
-            return RESPONSE_SERVER_ERROR;
-        }
-    }
 
 }
