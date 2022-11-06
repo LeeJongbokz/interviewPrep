0d6adc65ddfa9b534bf6d5f1c7d55ea069818c9e
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2022-11-06 14:25:29 +0900

    Feat: "router 수정"

--- frontend/src/component/Test.js
+++ frontend/src/component/Test.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
 import Problem from './Problem';
 // import Checkbox from './Checkbox';
 // import Card from './Card';
@@ -9,14 +8,12 @@ import Grid from '@mui/material/Unstable_Grid2';
 
 const Test = () => {
 
-  const { subject:testId } = useParams();
   const [question, setQuestion] = useState([]);//map은 array만 됨
   const navigate = useNavigate();
   
   useEffect(() => {
     const fetchQuestion = async () => {
       
-      console.log('api연동성공');
       const response = await fetch(`https://react-post-de8f7-default-rtdb.firebaseio.com/interviewPrep/question.json`);
 
       if(!response.ok){
@@ -24,7 +21,6 @@ const Test = () => {
       }
       const data = await response.json();
       setQuestion(data);
-      console.log(data);
 
     }
     fetchQuestion().catch((err) => {
@@ -50,7 +46,7 @@ const Test = () => {
       >
         {question.map((test, index) => {
             return (
-              <Grid key={index} item xs={12} sm={6} md={3} onClick={() => navigate(`/answer/${testId}`)}>
+              <Grid key={index} item xs={12} sm={6} md={3} onClick={() => navigate(`/test/${test.id}`)}>
                 <Problem problem={test} />
               </Grid>
             )
