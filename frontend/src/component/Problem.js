31bd6fea34fe89f3e6acd6506397d997bcfaebc6
lee jihong <vhfhfldi0303@hotelga.co.kr>
2022-11-11 16:01:22 +0900

    Feat: "문제 글자 수 가 30자 이상일 경우 말줄임표 표시 및 tooltip적용"

--- frontend/src/component/Problem.js
+++ frontend/src/component/Problem.js
@@ -1,8 +1,8 @@
-//import styled from 'styled-components';
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import {Button} from '@mui/material';
+import Tooltip from '@mui/material/Tooltip';
 import { useNavigate } from 'react-router-dom';
 const Problem = ({ problem }) => {
   const navigate = useNavigate();
@@ -13,11 +13,36 @@ const Problem = ({ problem }) => {
     >
       <CardContent>
         <Typography>
-          {problem.id +1+ '번'}
+          {problem.id + '번'}
         </Typography>
-        <Typography sx={{height:40 }} gutterBottom component="div">
-          문제: {problem.title || "-" }
-        </Typography>
+        <Tooltip title={problem.title || "-" } placement="bottom">
+          {
+            (function() {
+              if(problem.title.length > 30){
+                //console.log('ok ====' + problem.title.length)
+                return (
+                  <Typography 
+                    sx={{height:60 }} 
+                    gutterBottom component="div"
+                    >
+                    문제: {problem.title = problem.title.substr(0,problem.title.length - (problem.title.length - 30)) + '...' || "-" }
+                  </Typography>
+                );
+              }
+              // console.log(problem.title.length)
+              return (
+                <Typography 
+                  sx={{height:50 }} 
+                  gutterBottom component="div"
+                  >
+                  문제: {problem.title || "-"}
+                </Typography>
+              );
+              
+              
+            })()
+          }
+        </Tooltip>
         <Typography>
           Level : {problem?.level || "-" }
         </Typography>
@@ -33,11 +58,4 @@ const Problem = ({ problem }) => {
 };
 
 export default Problem;
-
-// const Card = styled.div`
-//   margin: 30px auto;
-//   width: 20%;
-//   border: 3px solid;
-//   border-radius: 15px;
-// `;
 
