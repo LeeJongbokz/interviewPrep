d820eec1f41a53d81ed616ffe52d1df1e99e83d8
N3theri9N <nnea5215@gmail.com>
2022-11-11 16:02:36 +0900

    signup paperui apply

--- frontend/src/component/Login.js
+++ frontend/src/component/Login.js
@@ -2,22 +2,18 @@ import React, { useRef, useContext } from 'react';
 import AuthContext from '../store/auth-context';
 import { useNavigate, Link } from 'react-router-dom';
 
+import PaperUI from "./UI/PaperUI";
 import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
-import Paper from '@mui/material/Paper';
-
-// import * as API from '../utils/api';
 
 const Login = () => {
   const navigate = useNavigate();
-  const authCtx = useContext(AuthContext)
+  const authCtx = useContext(AuthContext);
 
   const formRef = useRef();
   const emailRef = useRef();
@@ -25,24 +21,13 @@ const Login = () => {
 
   const onSubmitHandler = e => {
     e.preventDefault();
-    if(formRef.current.reportValidity() === false) {
-      return
+    if (formRef.current.reportValidity() === false) {
+      return;
     }
 
     authCtx.login(emailRef.current.value);
-    navigate("/");
+    navigate('/');
     return;
-    // try {
-    //   const res = API.post('login', {
-    //     email,
-    //     password,
-    //   });
-    //   if (res.status === 200) {
-    //     window.location.href = '/test';
-    //   }
-    // } catch (error) {
-    //   alert('이메일 혹은 비밀번호가 잘못 입력되었습니다.');
-    // }
   };
 
   // function ClickHander() {
@@ -52,62 +37,64 @@ const Login = () => {
   //   console.log(res);
   // }
   return (
-    <Container maxWidth="sm">
-      <Grid padding={4} component={Paper} square>
-        <Box
+    <PaperUI>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <Typography align="center" component="h1" variant="h5">
+          Login
+        </Typography>
+        <form onSubmit={onSubmitHandler} ref={formRef} noValidate>
+          <TextField
+            id="email"
+            label="email"
+            type="email"
+            margin="normal"
+            inputRef={emailRef}
+            required
+            fullWidth
+          />
+          <TextField
+            id="password"
+            label="passwords"
+            type="password"
+            margin="normal"
+            inputRef={passwordRef}
+            required
+            fullWidth
+          />
+          <ButtonGroup fullWidth>
+            <Button type="submit" variant="contained">
+              로그인
+            </Button>
+            <Button variant="outlined" component={Link} to="/signup">
+              회원가입
+            </Button>
+          </ButtonGroup>
+        </form>
+        <Divider variant="middle" />
+        <Stack
           sx={{
-            display: 'flex',
+            dislpay: 'flex',
             flexDirection: 'column',
           }}
+          mt={5}
+          spacing={1}
         >
-          <Typography align="center" component="h1" variant="h5">
-            Login
-          </Typography>
-          <form onSubmit={onSubmitHandler} ref={formRef} noValidate>
-            <TextField
-              id="email"
-              label="email"
-              type="email"
-              margin="normal"
-              inputRef={emailRef}
-              required
-              fullWidth
-            />
-            <TextField
-              id="password"
-              label="passwords"
-              type="password"
-              margin="normal"
-              inputRef={passwordRef}
-              required
-              fullWidth
-            />
-            <ButtonGroup fullWidth>
-              <Button type="submit" variant="contained">로그인</Button>
-              <Button variant="outlined" component={Link} to="/signup">회원가입</Button>
-            </ButtonGroup>
-          </form>
-          <Divider variant="middle" />
-          <Stack
-            sx={{
-              dislpay: 'flex',
-              flexDirection: 'column',
-            }}
-            mt={5}
-            spacing={1}
-          >
-            {/* <Button variant="outlined">kakao_login</Button>
+          {/* <Button variant="outlined">kakao_login</Button>
             <Button variant="outlined">naver_login</Button>
             <Button variant="outlined">google_login</Button> */}
-            {/* <Link onClick={ClickHander}> 
+          {/* <Link onClick={ClickHander}> 
                <SNSButton back={'/kakao_login.png'}></SNSButton> 
              </Link> 
             <SNSButton back={'/naver_login.png'}></SNSButton>
             <SNSButton back={'/google_login.png'}></SNSButton> */}
-          </Stack>
-        </Box>
-      </Grid>
-    </Container>
+        </Stack>
+      </Box>
+    </PaperUI>
   );
 };
 
--- frontend/src/component/SignUp.js
+++ frontend/src/component/SignUp.js
@@ -1,20 +1,14 @@
 import { useState } from 'react';
 
 import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button';
-// import ButtonGroup from '@mui/material/ButtonGroup';
-import Typography from "@mui/material/Typography";
-import Container from '@mui/material/Container';
-import Box from '@mui/material/Box'
-// import Stack from '@mui/material/Stack';
-// import Divider from '@mui/material/Divider';
-
-import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 
 // import * as API from '../utils/api';import { flexbox } from '@mui/system';
 // import styled from 'styled-components';
-import * as API from '../utils/api';
+// import * as API from '../utils/api';
+import PaperUI from './UI/PaperUI';
 
 const SignUp = () => {
   const [email, setEmail] = useState('');
@@ -28,17 +22,17 @@ const SignUp = () => {
     console.log(e.target.confirmpassword.value);
     console.log(e.target.name.value);
 
-    try {
-      const res = API.post('members/signup');
-      console.log(res);
-      console.log(HandleChange(e));
-    } catch (err) {
-      console.log(err);
-    }
+    // try {
+    //   const res = API.post('members/signup');
+    //   console.log(res);
+    //   console.log(HandleChange(e));
+    // } catch (err) {
+    //   console.log(err);
+    // }
   };
 
   function HandleChange(e) {
-    console.log(onSubmit(e));
+  //  console.log(onSubmit(e));
     switch (e.target.name) {
       case 'name':
         setName(e.target.value);
@@ -56,84 +50,70 @@ const SignUp = () => {
     }
   }
   return (
-    <>
-      <Container maxWidth="sm">
-        <Grid
-          padding={4}
-          component={Paper}
-          square
+    <PaperUI>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <Typography align="center" component="h1" variant="h5">
+          Sign Up
+        </Typography>
+        <form
+          onSubmit={onSubmit}
+          noValidate
         >
-          <Box 
-            sx={{
-              display: "flex",
-              flexDirection: 'column',
-            }}
-            >
-              <Typography align='center' component="h1" variant="h5">
-              Sign Up
-            </Typography>
-            <form
-            // onChange={e => HandleChange(e)} 
-            noValidate
-            >
-              <Typography align='center' component="h4" variant="h5">
-                이름
-              </Typography>
-              <TextField 
-                id="name" 
-                label="name"
-                type="text" 
-                margin="normal"
-                defaultValue={name}
-                required
-                fullWidth
-                placeholder="Enter Name"
-              />
-              <Typography align='center' component="h4" variant="h5">
-                이메일
-              </Typography>
-              <TextField
-                type="email"
-                id="email"
-                name="email"
-                placeholder="email"
-                defaultValue={email}
-                margin="normal"
-                required
-                fullWidth
-              />
-              <Typography align='center' component="h4" variant="h5">
-                비밀번호
-              </Typography>
-              <TextField
-                type="password"
-                id="password"
-                name="password"
-                placeholder="Password"
-                defaultValue={password}
-                margin="normal"
-                required
-                fullWidth
-              />
-              <Typography align='center' component="h4" variant="h5">
-                비밀번호 확인
-              </Typography>
-              <TextField
-                type="password"
-                id="confirmpassword"
-                name="confirmpassword"
-                placeholder="Password"
-                defaultValue={confirmPassword}
-                margin="normal"
-                required
-                fullWidth
-              />
-              <Button variant="contained" label={'margin="normal"'}>회원가입</Button>
-            </form>
-          </Box>
-        </Grid>
-      </Container>
-    </>
+          <TextField
+            id="name"
+            label="name"
+            name="name"
+            type="text"
+            margin="normal"
+            required
+            fullWidth
+            value={name}
+            onChange={HandleChange}
+          />
+          <TextField
+            id="email"
+            label="email"
+            type="email"
+            margin="normal"
+            name="email"
+            required
+            fullWidth
+            value={email}
+            onChange={e => HandleChange(e)}
+          />
+          <TextField
+            id="password"
+            label="password"
+            type="password"
+            margin="normal"
+            name="password"
+            required
+            fullWidth
+            value={password}
+            onChange={e => HandleChange(e)}
+          />
+          <TextField
+            id="confirmpassword"
+            label="confirmpassword"
+            type="password"
+            margin="normal"
+            name="confirmpassword"
+            required
+            fullWidth
+            value={confirmPassword}
+            onChange={e => HandleChange(e)}
+          />
+          <Button type="submit" variant="contained" label={'margin="normal"'}>
+            회원가입
+          </Button>
+        </form>
+      </Box>
+    </PaperUI>
   );
 };
 
