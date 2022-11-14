10db8660cc9514a4b4d8f8271153880caa716fac
N3theri9N <nnea5215@gmail.com>
2022-11-14 23:09:13 +0900

    pagination & refactoring

--- frontend/src/component/ProblemList.js
+++ frontend/src/component/ProblemList.js
@@ -1,11 +1,9 @@
-import React, { useEffect,useState} from 'react';
+// import React, { useEffect,useState} from 'react';
 import Problem from './Problem';
-import Skeleton from '@mui/material/Skeleton';
 import Grid from '@mui/material/Unstable_Grid2';
 
-
-const ProblemList = ({question}) => {
-  //const [question, setQuestion] = useState([]);//map은 array만 됨
+const ProblemList = (props) => {
+  // const [question, setQuestion] = useState([]);//map은 array만 됨
   // useEffect(() => {
   //   const fetchQuestion = async () => {
   //     setLoading(true);
@@ -13,27 +11,32 @@ const ProblemList = ({question}) => {
   //     //console.log(target)
   //     const response = await fetch(`http://52.202.27.18:8080/question/${target}`);
 
+  //     if(!response.ok){
+  //       throw new Error('Some Thing Went Error');
+  //     }
+  //     const data = await response.json();
+  //     setQuestion(data.content);
+  //   }
+  //   fetchQuestion().catch((err) => {
+  //     console.log(err)
+  //   })
+  //   setLoading(false);
+  // }, [searchtype]);
+
   return (
-    <Grid container spacing={1} columns={12}>
-      {question.map((test, index) => {
+    <Grid 
+        container 
+        spacing={1} 
+        columns={{ xs:12 }}
+      >
+      {props.question.map((test, index) => {
         return (
-          <Grid        
-            item
-            key={index}
-            xs={12}
-            sm={6}
-            md={3}
-            // sx={{ width: { xs: '100%', sm: '50%', md: '25%' } }}
-          >
-            <Problem
-              sx={{ height: { xs: '250px', sm: '150px', md: '100px' } }}
-              key={test.id}
-              problem={test}
-            />
+          <Grid key={index} item xs={12} sm={6} md={3} sx={{width:{xs:"100%", sm:"50%", md:"25%"}}}>
+            <Problem sx={{height:{xs:"250px", sm:"150px", md:"100px"}  }} key={test.id} problem={test} />
           </Grid>
-        );
+        )
       })}
-    </Grid>
+      </Grid>
   );
 };
 
--- frontend/src/component/Test.js
+++ frontend/src/component/Test.js
@@ -1,38 +1,75 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import ProblemList from './ProblemList';
 import Select from './Select';
 import ContainerUI from './UI/ContainerUI';
+import Skeleton from '@mui/material/Skeleton';
+import Pagination from '@mui/material/Pagination';
+import Box from '@mui/material/Box';
 
 const Test = () => {
   const [question, setQuestion] = useState([]);
-  const [searchtype, setsearchtype] = useState('all');//type의 값을 설정
+  const [searchType, setSearchType] = useState('all'); //type의 값을 설정
+  const [loading, setLoading] = useState(true);
+  const [totalPage, setTotalPage] = useState(0);
+  const [page, setPage] = useState(0);
 
   //선택했을 때 카테고리 값을 받아와서 업데이트 해줄 것
-  const onSelect = useCallback(searchtype => setsearchtype(searchtype),[]);
-  //console.log(searchtype)
+  // const selectTypeHandler = useCallback((event) => {
+  //   const searchtype = event.target.value;
+  //   setsearchtype(searchtype);
+  // }, []);
+
+  const selectTypeHandler = event => {
+    setSearchType(event.target.value);
+  };
+
+  //console.log(searchType)
+  const pageHandler = (event, value) => {
+    setPage(value-1);
+  };
 
   useEffect(() => {
     const fetchQuestion = async () => {
-      const response = await fetch(`http://52.202.27.18:8080/question/`);
-      if(!response.ok){
+      setLoading(true);
+      setQuestion([]);
+      // const target = searchType === [] ? '' : `${searchType}?page=${page}`;
+      const response = await fetch(`http://52.202.27.18:8080/question/${searchType}?page=${page}`);
+
+      if (!response.ok) {
         throw new Error('Some Thing Went Error');
       }
       const data = await response.json();
       setQuestion(data.content);
-    }
-    fetchQuestion().catch((err) => {
-      console.log(err)
-    })
-  },[]);
+      setTotalPage(data.totalPages);
+      setLoading(false);
+    };
+
+    fetchQuestion().catch(err => {
+      console.log(err);
+      setLoading(false);
+    });
+  }, [searchType, page]);
 
   return (
     <ContainerUI>
       <div>
-        <Select categories={question} onSelect={onSelect} searchtype={searchtype}/>
+        <Select
+          // categories={question}
+          onSelect={selectTypeHandler}
+          searchType={searchType}
+        />
       </div>
       <div>
-        <ProblemList searchtype={searchtype}/>
+        {loading && (
+          <Skeleton variant="rounded" sx={{ height: { xs: '300px', sm: '200px', md: '150px' } }}>
+            LOADING!
+          </Skeleton>
+        )}
+        {!loading && <ProblemList question={question} />}
       </div>
+      <Box margin={5} display="flex" justifyContent="center" alignItems="center">
+        <Pagination count={totalPage} onChange={pageHandler} color="primary" />
+      </Box>
     </ContainerUI>
   );
 };
