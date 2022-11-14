2d87a720db11d6a259c4b153c27c1c8814cd7e87
N3theri9N <nnea5215@gmail.com>
2022-11-14 23:09:13 +0900

    pagination & refactoring

--- frontend/src/component/Select.js
+++ frontend/src/component/Select.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
@@ -6,47 +6,47 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
-export default function BasicSelect({categories, onSelect, searchtype}) {
-  const [categoryName, setcategoryName] = useState('');
-  let array = [];
-  let type;
+const CATEGORIES = ['java', 'os', 'spring'];
+
+export default function BasicSelect(props) {
+  //const [categoryName, setcategoryName] = useState('');
+  // let array = [];
+  // let type = CATEGORIES;
+  // let type = ;
 
   //category type 받아옴.(새로운 문제 type을 추가하더라도 코드 수정 없도록 하기 위해 데이터를 받아오는 형식으로 함)
-  categories.map((category) => {
-    array.push(category.type);
-  })
-  type = array.filter(function(v,i){
-    return array.indexOf(v) === i;
-  });
+  // CATEGORIES.map((category) => {
+  //   array.push(category.type);
+  // })
+  // type = array.filter(function(v,i){
+  //   return array.indexOf(v) === i;
+  // });
 
-  const handleChange = (event) => {
-    const target = event.target.value;
-    setcategoryName(target);
-    //해당 카테고리를 클릭했을 때 해당 카테고리의 type을 onSelect의 파라미터로 가져가서 카테고리 값을 업데이트 시킴 
-    onSelect(target);
-
-  };
+  // const handleChange = (event) => {
+  //   const target = event.target.value;
+  //   setcategoryName(target);
+  //   //해당 카테고리를 클릭했을 때 해당 카테고리의 type을 onSelect의 파라미터로 가져가서 카테고리 값을 업데이트 시킴
+  //   onSelect(target);
+  // };
 
   return (
-    <Box sx={{ mt:1 ,mb:1, width: 250 , backgroundColor: "white"}}>
+    <Box sx={{ mt: 1, mb: 1, width: 250, backgroundColor: 'white' }}>
       <FormControl fullWidth>
         <InputLabel id="demo-simple-select-label">category</InputLabel>
         <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
-          value={categoryName}
+          value={props.searchType}
           label="category"
-          onChange={handleChange}
-
-        > 
-          {type.map((item,index) => {
-            //console.log(searchtype)
+          onChange={props.onSelect}
+        >
+          <MenuItem value="all">all</MenuItem>
+          {CATEGORIES.map((item, index) => {
             return (
-              <MenuItem 
-                key={index} 
-                value={item}
-              >{item}</MenuItem>
-            )
+              <MenuItem key={index} value={item}>
+                {item}
+              </MenuItem>
+            );
           })}
         </Select>
       </FormControl>
