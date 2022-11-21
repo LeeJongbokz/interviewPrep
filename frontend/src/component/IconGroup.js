f5075ab793e4d165a4a198a92967d5c874a92e16
N3theri9N <nnea5215@gmail.com>
2022-11-21 15:16:50 +0900

    middle align Icon group

--- frontend/src/component/IconGroup.js
+++ frontend/src/component/IconGroup.js
@@ -4,15 +4,15 @@ import IconButton from '@mui/material/IconButton';
 
 const IconGroup = () => {
   return (
-    <Stack direction="row">
+    <Stack direction="row" justifyContent="space-evenly">
       <IconButton component={Link} to="#">
-        <img src="./logo/google-logo.svg" alt="google" style={{ width: 30 }} />
+        <img src="./logo/google-logo.svg" alt="google" style={{ width: 50 }} />
       </IconButton>
       <IconButton component={Link} to="#">
-        <img src="./logo/naver-logo.svg" alt="naver" style={{ width: 30 }} />
+        <img src="./logo/naver-logo.svg" alt="naver" style={{ width: 50 }} />
       </IconButton>
       <IconButton component={Link} to="#">
-        <img src="./logo/kakaotalk-logo.svg" alt="kakaotalk" style={{ width: 30 }} />
+        <img src="./logo/kakaotalk-logo.svg" alt="kakaotalk" style={{ width: 50 }} />
       </IconButton>
     </Stack>
   );
