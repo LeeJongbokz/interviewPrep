abe0449da3723cc9b4296c678af99b748d2c50f3
lee-ji-hong <zhffktkdlekghksxk@naver.com>
2022-11-27 14:38:37 +0900

    Feat: "모의고사 버튼 생성"

--- frontend/src/component/Exam/ExamList.js
+++ frontend/src/component/Exam/ExamList.js
@@ -1,8 +1,19 @@
+import ContainerUI from '../UI/ContainerUI';
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
 const ExamList = () => {
+  const navigate = useNavigate();
+
     return (
-      <>
+      <ContainerUI>
       <div>모의고사 페이지입니다</div>
-      </>
+      <Button variant="outlined" onClick={() => navigate('/')}>
+        FRONTEND
+      </Button>
+      <Button variant="outlined" onClick={() => navigate('/')}>
+        BACKEND
+      </Button>
+      </ContainerUI>
     );
   };
   
