994a4208b3d0f5e3ebc3c64130b41c79c4526539
JT <38105420+wjdxor@users.noreply.github.com>
2022-12-18 14:33:58 +0900

    fix: examId 컬럼명 추가

--- backend/src/main/java/com/example/interviewPrep/quiz/exam/domain/Exam.java
+++ backend/src/main/java/com/example/interviewPrep/quiz/exam/domain/Exam.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -26,6 +27,7 @@ import static javax.persistence.FetchType.LAZY;
 public class Exam {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "EXAM_ID")
     private Long id;
 
     private Long kitId;
